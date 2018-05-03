a51c6b94e02334c7bb37612d7c1aae730d46ff04
Benoit Pingris <benoit.pingris@epitech.eu>
2018-05-03 15:38:53 +0200

    trade

--- trade.py
+++ trade.py
@@ -2,15 +2,74 @@
 
 import requests
 import csv
+import numpy as np
 import matplotlib.pyplot as plt
+from pprint import pprint
+import time
 
-datas = []
 
-with open('datas/NFLX.csv', 'r') as csvfile:
-	csv_reader = csv.reader(csvfile)
-	next(csv_reader)
-	for line in csv_reader:
-		datas.append(float(line[1]))
+class Datas:
+	"""
+	Datas class
+	"""
+	def __init__(self, filename):
+		"""
+		Init Function
+		:param filename: file with datas
+		"""
 
-plt.plot(datas)
-plt.show()
+		self.filename = filename
+		self.dates = []
+		self.prices = []
+		self.example = []
+		self.get_datas()
+
+
+	def get_datas(self):
+		"""
+		get datas from csv file
+		"""
+		with open(self.filename, 'r') as csv_file:
+			csv_reader = csv.reader(csv_file)
+			next(csv_reader)
+			for row in csv_reader:
+				self.dates.append(row[0])
+				self.prices.append(float(row[1]))
+
+
+	def calc_avg(self, prices):
+		return sum(prices) / len(prices)
+
+
+def main():
+	datas = Datas("./datas/NFLX.csv")
+
+	# -1 UNDEF
+	# 0 BUY
+	# 1 SELL
+	state = -1
+
+	money = 1000.0
+	_list = []
+	i = 0
+	print("Money at start: %.2f$" % money)
+	while True:
+		if (i < len(datas.prices)):
+			_list.append(datas.prices[i])
+		else:
+			break
+		avg = datas.calc_avg(_list)
+		if _list[i] > avg and state != 0:
+			print("SELL")
+			money += _list[i]
+			state = 0
+		elif _list[i] < avg and state != 1:
+			print("BUY")
+			money -= _list[i]
+			state = 1
+		i += 1
+	print("Money at end: %.2f$" % money)
+
+
+if __name__ == '__main__':
+	main()
